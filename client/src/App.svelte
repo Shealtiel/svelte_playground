1cd5dfb19eadc427fd83175a5a3841866f7b36d5
Alexander Shtaub <alexander.shtaub@station-hotels.ru>
2019-12-30 17:31:51 +0200

    init

---
+++ client/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { writable } from "svelte/store";
+
+  let counter = writable(2);
+  let array;
+  $: array = [...Array($counter).keys()];
+</script>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .child {
+    margin: 1px;
+    color: white;
+    text-align: center;
+    background-color: black;
+  }
+</style>
+
+<main>
+  <input type="range" bind:value={$counter} />
+  {#each array as item}
+    <div class="child" style="flex: {item}">{item}</div>
+  {/each}
+</main>
